a8a6294e69e507c1a76456b9e5f57c4d87f96c49
Rafael Rocha Rosas <rafae@MacBook-Air-de-PABLO.local>
2026-03-01 17:21:38 -0600

    se introduce una composicion en Empleado

--- src/clasesYObjetos/pruebaempleados.java
+++ src/clasesYObjetos/pruebaempleados.java
@@ -4,8 +4,10 @@ public class pruebaempleados {
 
     public static void main (String[] args){
 
+        Nacionalidad nacionalidad = new Nacionalidad(7, "mexicana");
+
         Empleado empleado1 = new Empleado ();
-        Empleado empleado2 = new Empleado ("Rafa", "123-123", 1234);
+        Empleado empleado2 = new Empleado ("Rafa", "123-123", 1234, nacionalidad);
 
         empleado1.incrementarSalario(200);
         empleado2.incrementarSalario(200);
---
+++ src/pokemon/Principal.java
@@ -0,0 +1,13 @@
+package pokemon;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        Pkmn pokemon = new Pkmn();
+    }
+
+}
